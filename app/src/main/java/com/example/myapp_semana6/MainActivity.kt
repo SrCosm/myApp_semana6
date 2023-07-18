ca1e825aac2261c5ce3929f885b26603ed93259e
SrCosm <jalonsoaa2004@gmail.com>
2023-07-17 20:31:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapp_semana6/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapp_semana6
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.inicio_sesion)
+        setContentView(R.layout.pag_principal)
+    }
+
+}
